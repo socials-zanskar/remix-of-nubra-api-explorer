ba723608abada9d2ccd805746adcf860e420b626
akshayn-spec <akshay@zanskar.xyz>
2026-01-08 16:16:58 +0530

    Vite.config update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { blogDiscoveryPlugin } from "./vite-plugins/blog-discovery";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/products/api',
   server: {
     host: "::",
     port: 8080,
